2c4504700d9a917ac47a5818571322e015259b6a
Alexander Bendera <beggel38@gmail.com>
2026-06-01 16:47:41 +0300

    data quality tests (SODA) + unit + integration tests

--- dags/main.py
+++ dags/main.py
@@ -2,8 +2,8 @@ from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
-
 from datawarehouse.dwh import staging_table, core_table
+from dataquality.soda import yt_elt_data_quality
 
 
 local_tz = pendulum.timezone("Europe/Malta")
@@ -22,6 +22,9 @@ default_args = {
   # "end_date": datetime(2030, 12, 31, tzinfo=local_tz)
 }
 
+staging_schema = "staging"
+core_schema = "core"
+
 with DAG(
   dag_id="produce_json",
   default_args=default_args,
@@ -39,10 +42,11 @@ with DAG(
   # Define dependencies
   playlist_id >> video_ids >> extracted_data >> save_to_json_task
 
+
 with DAG(
   dag_id="update_db",
   default_args=default_args,
-  description="DAG to process KSON file and insert data into both staging and core schemas",
+  description="DAG to process JSON file and insert data into both staging and core schemas",
   schedule="0 15 * * *",
   catchup=False
 ) as dag:
@@ -53,3 +57,19 @@ with DAG(
 
   # Define dependencies
   update_staging >> update_core
+
+
+with DAG(
+  dag_id="data_quality",
+  default_args=default_args,
+  description="DAG to run data quality checks for both STAGING and CORE schemas",
+  schedule="0 16 * * *",
+  catchup=False
+) as dag:
+  
+  # Define task
+  soda_validate_staging = yt_elt_data_quality(staging_schema)
+  soda_validate_core = yt_elt_data_quality(core_schema)
+
+  # Define dependencies
+  soda_validate_staging >> soda_validate_core
---
+++ tests/unit_test.py
@@ -0,0 +1,55 @@
+def test_api_key(api_key):
+  assert api_key == "MOCK_KEY1234"
+
+def test_channel_handle(channel_handle):
+  assert channel_handle == "MRCHEES"
+
+def test_postgres_conn(mock_postgres_conn_vars):
+  conn = mock_postgres_conn_vars
+
+  assert conn.login == "mock_username"
+  assert conn.password == "mock_password"
+  assert conn.host == "mock_host"
+  assert conn.port == 1234
+  assert conn.schema == "mock_db_name"
+
+def test_dags_integrity(dagBag):
+  # 1.
+  print("====================")
+  print(dagBag.import_errors)
+  assert dagBag.import_errors == {}, f"Import errors found: {dagBag.import_errors}"
+  
+  # 2.
+  expected_dag_ids = ["produce_json", "update_db", "data_quality"]
+  loaded_dag_ids = list(dagBag.dags.keys())
+
+  print("====================")
+  print(dagBag.dags.keys())
+  for dag_id in expected_dag_ids:
+    assert dag_id in loaded_dag_ids, f"DAG {dag_id} is missing."
+  
+  # 3.
+  print("====================")
+  print(dagBag.size())
+  assert dagBag.size() == 3
+  
+  # 4.
+  expected_task_count = {
+    "produce_json": 4,
+    "update_db": 2,
+    "data_quality": 2
+  }
+  print("====================")
+  
+  for dag_id, dag in dagBag.dags.items():
+    expected_count = expected_task_count[dag_id]
+    actual_count = len(dag.tasks)
+
+    assert (
+      expected_count == actual_count
+    ), f"DAG {dag_id} has {actual_count} tasks, expected {expected_count}."
+    print(dag_id, len(dag.tasks))
+
+
+
+
